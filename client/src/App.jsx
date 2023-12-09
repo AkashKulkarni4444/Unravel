449c7c0d6b60a6f21bbd9af0aabf57c139ad3637
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-12-09 21:20:10 +0530

    dashboard done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import RootLayout from './pages/RootLayout';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/dashboard/Layout/Layout';
+import Dashboard from './pages/dashboard/Dashboard/Dashboard';
+// import BoardPage from './pages/dashboard/Board/Board';
+import Calendar from './pages/dashboard/Calendar/Calendar';
+// import DataGrid from './pages/dashboard/DataGrid/DataGrid';
 
 function App() {
 
@@ -11,8 +16,19 @@ function App() {
         // {path: '/', element: <Homepage/> },
         // {path: '/ContactUs', element: <ContactUs/> },
         // {path: '/wishlist', element: <WishlistPage/>},
+        // {path: '/wishlist', element: <WishlistPage/>},
       ]
     },  
+    {
+      path: "/dashboard",
+      element: <Layout />,
+      children: [
+        { path: "home", element: <Dashboard /> },
+        { path: "calendar", element: <Calendar /> },
+        // { path: "board", element: <BoardPage /> },
+        // { path: "/users", element: <DataGrid /> },
+      ],
+    },
   ]);
 
   return (
